e4fb20aa1d6de44aa707af9a02def0c35d79682e
a72481 <a72481@alunos.uminho.pt>
2023-05-01 02:23:27 +0100

    Adicionadas funções calc_dist e reset_dist.

--- codigo/main.c
+++ codigo/main.c
@@ -7,11 +7,58 @@
 #include "state.h"
 #include "mapa.h"
 
+#define MAX_DIST 19
+#define MAX_ROW 60
+#define MAX_COL 250
+#define SHOW_DIST 1
+
 void draw_player (STATE *st) {
 	attron(COLOR_PAIR(COLOR_WHITE));
 	mvaddch(st->playerX, st->playerY, '@' | A_BOLD);
 	attroff(COLOR_PAIR(COLOR_WHITE));
 }
+
+void reset_dist (STATE * st) {
+	int i, j;
+
+	for (i = 0; i < MAX_ROW; i++){
+		for (j = 0; j < MAX_COL; j++)
+		st->map[i][j].dist = -1;
+	}
+}
+
+void calc_dist (int R, int C, int value, STATE * st) {
+	if (R < 0 || C < 0 || R > MAX_ROW || C > MAX_COL) return;
+	if (value > MAX_DIST) return;
+	if (st->map[R][C].dist != (-1) && st->map[R][C].dist <= value) return;
+	if (st->map[R][C].caracterAtual == '#') return;
+
+	st->map[R][C].dist = value;
+	
+	// A flag SHOW_DIST permite mostrar ou esconder as distâncias. Deve-se comentar a função draw_light para não se sobrepor.
+	if (SHOW_DIST && value != 0) {
+		if (value < 10) {
+			attron(COLOR_PAIR(COLOR_MAGENTA));
+			mvaddch(R, C, (value + '0'));
+			attroff(COLOR_PAIR(COLOR_MAGENTA));
+		}
+		else {
+			attron(COLOR_PAIR(COLOR_CYAN));
+			mvaddch(R, C, ((value % 10) + '0'));
+			attroff(COLOR_PAIR(COLOR_CYAN));
+		}
+	}
+
+	calc_dist (R+1, C+1, value+1, st);
+	calc_dist (R, C+1, value+1, st);
+	calc_dist (R-1, C+1, value+1, st);
+	calc_dist (R-1, C, value+1, st);
+	calc_dist (R-1, C-1, value+1, st);
+	calc_dist (R, C-1, value+1, st);
+	calc_dist (R+1, C-1, value+1, st);
+	calc_dist (R+1, C, value+1, st);
+}
+
 
 void draw_light (STATE *st) {
 	attron(COLOR_PAIR(COLOR_YELLOW));
@@ -25,6 +72,7 @@ void draw_light (STATE *st) {
 	if (st->map[st->playerX + 1][st->playerY + 1].caracterAtual != '#') mvaddch(st->playerX + 1, st->playerY + 1, '.' | A_BOLD);
     attroff(COLOR_PAIR(COLOR_YELLOW));
 }
+
 
 void draw_info (STATE *st) {
 	move(st->nROWS - 1, 0);
@@ -111,8 +159,13 @@ int main() {
 	keypad(stdscr, true);
 
 	init_pair(COLOR_WHITE, COLOR_WHITE, COLOR_BLACK);
-        init_pair(COLOR_YELLOW, COLOR_YELLOW, COLOR_BLACK);
-        init_pair(COLOR_BLUE, COLOR_BLUE, COLOR_BLACK);
+	init_pair(COLOR_YELLOW, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(COLOR_BLUE, COLOR_BLUE, COLOR_BLACK);
+	init_pair(COLOR_GREEN, COLOR_GREEN, COLOR_BLACK);
+	init_pair(COLOR_RED, COLOR_RED, COLOR_BLACK);
+	init_pair(COLOR_MAGENTA, COLOR_MAGENTA, COLOR_BLACK);
+	init_pair(COLOR_CYAN, COLOR_CYAN, COLOR_BLACK);
+
 
 	gerar(&st);
 
@@ -120,6 +173,8 @@ int main() {
 		draw_map(&st);
 		draw_info(&st);
 		draw_player(&st);
+		reset_dist(&st);
+		calc_dist(st.playerX, st.playerY, 0, &st);
 		draw_light (&st);
 		move(st.playerX, st.playerY);
 		update(&st);
